6af111020b7b5de424d39f7a1fb00a0dfd2cf7c9
augustofrade <aquaaticnaotrava@gmail.com>
2026-03-18 22:29:46 -0300

    Add src/index file & Fixes to package.json and tsconfig

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { inject } from "./common";
+import { DI } from "./core";
+import { InjectableSingleton, InjectableTransient } from "./core/decorators";
+
+export { DI, inject, InjectableSingleton, InjectableTransient };
